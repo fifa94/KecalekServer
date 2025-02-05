54a277c2d71000ef1d87b548590c7078ed8afa6f
Filip Šrámek <sramek.filip@gmail.com>
2025-02-05 20:22:04 +0100

    Added tcp server generated from AI

---
+++ CommunicatonServer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TcpServer
+{
+    private const int Port = 12345;
+
+    public async Task StartAsync()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, Port);
+        listener.Start();
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            _ = HandleClientAsync(client); // Spustíme asynchronní úlohu pro obsluhu klienta
+        }
+    }
+
+    private async Task HandleClientAsync(TcpClient client)
+    {
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+            {
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Received: {message}");
+
+                // Zpětná odpověď klientovi
+                string response = $"Echo: {message}";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+            }
+        }
+
+        client.Close();
+    }
+}
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        TcpServer server = new TcpServer();
+        await server.StartAsync(); // Spustíme server asynchronně
+    }
+}
